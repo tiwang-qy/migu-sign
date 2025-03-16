f9eaa3175d6d215ef2b754e6d09e0d01f706f95d
tiwang-qy <tian_wang@hotmail.com>
2025-03-17 00:13:45 +0800

    Update migu.sh

--- migu.sh
+++ migu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash 
 
-curl -H "Cookie: ${COOKIE}" -H "Content-Type: application/json; charset=UTF-8" -X POST -d '{}' 'https://gw.aikan.miguvideo.com/ygw/api/dispatch/migu-activity-custom/sign/sendAward'
+curl -H "Cookie: ${COOKIE}" -H "Content-Type: application/json; charset=UTF-8" -X POST -d '{}' 'https://gw.aikan.miguvideo.com/ygw/api/dispatch/energy-center/energy/uploadNew'
 
